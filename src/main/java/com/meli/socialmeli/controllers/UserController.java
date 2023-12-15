963d42d505f135b816cb26b5cbbf1adacfb74c92
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2023-12-15 17:21:32 -0600

    Add controller method US 007

--- src/main/java/com/meli/socialmeli/controllers/UserController.java
+++ src/main/java/com/meli/socialmeli/controllers/UserController.java
@@ -1,9 +1,24 @@
 package com.meli.socialmeli.controllers;
 
+import com.meli.socialmeli.services.IUserService;
+import com.meli.socialmeli.services.impl.UserServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    IUserService userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
+    public ResponseEntity<?> unfollowUser(int userId, int userIdToUnfollow){
+        return ResponseEntity.ok(userService.unfollowUser(userId, userIdToUnfollow));
+    }
 }
