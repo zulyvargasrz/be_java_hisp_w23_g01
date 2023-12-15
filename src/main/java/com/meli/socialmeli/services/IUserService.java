393c7de1a8630f83c8ee71dae0997f05a989ccf6
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2023-12-15 17:32:36 -0600

    Add abstract service method US 007

--- src/main/java/com/meli/socialmeli/services/IUserService.java
+++ src/main/java/com/meli/socialmeli/services/IUserService.java
@@ -1,4 +1,8 @@
 package com.meli.socialmeli.services;
 
+import com.meli.socialmeli.dtos.UserUnfollowDto;
+
 public interface IUserService {
+    UserUnfollowDto unfollowUser(int userId, int userIdToUnfollow);
+
 }
