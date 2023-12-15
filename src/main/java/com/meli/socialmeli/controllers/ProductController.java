68dfc0553125c342c35d547abbfa9cf4910a1b75
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-15 17:05:27 -0600

    Add method to handle US006 on product controller
    Used dependency inyeccion required to use the service of products.
    
    The return value is of type ResponseEntity<PostsFromFollowsDTO> and not of type
    ResponseEntity<?> for more clarity.

--- src/main/java/com/meli/socialmeli/controllers/ProductController.java
+++ src/main/java/com/meli/socialmeli/controllers/ProductController.java
@@ -1,9 +1,24 @@
 package com.meli.socialmeli.controllers;
 
+import com.meli.socialmeli.dtos.response.PostsFromFollowsDTO;
+import com.meli.socialmeli.services.IProductService;
+import com.meli.socialmeli.services.impl.ProductServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final IProductService iProductService;
+    public ProductController(ProductServiceImpl productService){
+        this.iProductService = productService;
+    }
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<PostsFromFollowsDTO> getAllPostsFollowsLastTwoWeeks(@PathVariable Integer userId) {
+        return new ResponseEntity<>(iProductService.getAllPostsFollowsLastTwoWeeks(userId), HttpStatus.OK);
+    }
 }
