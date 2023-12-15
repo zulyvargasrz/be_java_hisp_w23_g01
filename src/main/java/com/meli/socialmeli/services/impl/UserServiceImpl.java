3e1c303181bf53f09c5082e0c166051333e9810e
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2023-12-15 17:33:52 -0500

    Add service method US 003 List Followers user founded
    Find followers, map to userFollowersDto and return

--- src/main/java/com/meli/socialmeli/services/impl/UserServiceImpl.java
+++ src/main/java/com/meli/socialmeli/services/impl/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.meli.socialmeli.services.impl;
 
 import com.meli.socialmeli.dtos.response.UserFollowersDTO;
+import com.meli.socialmeli.dtos.response.UserInfoDTO;
 import com.meli.socialmeli.entities.User;
 import com.meli.socialmeli.exceptions.custom.NotFoundException;
 import com.meli.socialmeli.repositories.IUserRepository;
 import com.meli.socialmeli.services.IUserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,7 +24,12 @@ public class UserServiceImpl implements IUserService {
     public UserFollowersDTO findFollowersById(int id) {
         Optional<User> userFound = userRepository.finById(id);
         if (userFound.isEmpty()) throw new NotFoundException("There is no user with the id: " + id);
-        
-        return null;
+
+        List<UserInfoDTO> followers = userFound.get().getFollowers()
+                                                     .stream()
+                                                     .map( f -> new UserInfoDTO(f.getUser_id(), f.getUser_name() ) )
+                                                     .toList();
+
+        return new UserFollowersDTO(userFound.get().getUser_id(), userFound.get().getUser_name(), followers);
     }
 }
