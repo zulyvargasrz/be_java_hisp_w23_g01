ef1e3dfde5e3022fde7773baa1ab47c53662969b
Tonatiuh Díaz <alfredo.diazgomez@mercadolibre.com.mx>
2023-12-15 16:05:08 -0600

    Created followSeller() service signature

--- src/main/java/com/meli/socialmeli/services/IUserService.java
+++ src/main/java/com/meli/socialmeli/services/IUserService.java
@@ -1,4 +1,7 @@
 package com.meli.socialmeli.services;
 
+import com.meli.socialmeli.dtos.MessageDto;
+
 public interface IUserService {
+    MessageDto followSeller(int userId, int userIdToFollow);
 }
--- src/main/java/com/meli/socialmeli/services/impl/UserServiceImpl.java
+++ src/main/java/com/meli/socialmeli/services/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.meli.socialmeli.services.impl;
 
+import com.meli.socialmeli.dtos.MessageDto;
 import com.meli.socialmeli.services.IUserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements IUserService {
+    @Override
+    public MessageDto followSeller(int userId, int userIdToFollow) {
+        return null;
+    }
 }
