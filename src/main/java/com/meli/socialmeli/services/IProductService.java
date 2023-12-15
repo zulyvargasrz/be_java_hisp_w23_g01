86dae675721e3688be3d6c47025fd5b1096267a2
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-15 17:17:01 -0600

    Add method signature to return DTO of US006

--- src/main/java/com/meli/socialmeli/services/IProductService.java
+++ src/main/java/com/meli/socialmeli/services/IProductService.java
@@ -1,4 +1,7 @@
 package com.meli.socialmeli.services;
 
+import com.meli.socialmeli.dtos.response.PostsFromFollowsDTO;
+
 public interface IProductService {
+    PostsFromFollowsDTO getAllPostsFollowsLastTwoWeeks(Integer userId);
 }
