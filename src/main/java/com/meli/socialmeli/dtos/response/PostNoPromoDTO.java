ea10eba46cf140fa08ec895b076d2565e2e551fa
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-15 17:20:28 -0600

    Add DTOS for US006

---
+++ src/main/java/com/meli/socialmeli/dtos/response/PostNoPromoDTO.java
@@ -0,0 +1,8 @@
+package com.meli.socialmeli.dtos.response;
+public class PostNoPromoDTO {
+    private Integer post_id;
+    private String date;
+    private ProductDTO product;
+    private Integer category;
+    private Double price;
+}
