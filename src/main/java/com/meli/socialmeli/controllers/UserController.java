af8e5246ca718157fd72851792348cc4f977504b
Tonatiuh Díaz <alfredo.diazgomez@mercadolibre.com.mx>
2023-12-15 15:51:05 -0600

    Create void endpoint to follow a seller

--- src/main/java/com/meli/socialmeli/controllers/UserController.java
+++ src/main/java/com/meli/socialmeli/controllers/UserController.java
@@ -1,9 +1,18 @@
 package com.meli.socialmeli.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<?> followSeller(@PathVariable int userId, @PathVariable int userIdToFollow){
+        return ResponseEntity.ok(null);
+    }
+
 }
