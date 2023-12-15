e7b6022adcc4410fc2d043c05c1974f1e1c8863b
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-15 17:19:17 -0600

    Placeholder method implementation service US006

--- src/main/java/com/meli/socialmeli/services/impl/ProductServiceImpl.java
+++ src/main/java/com/meli/socialmeli/services/impl/ProductServiceImpl.java
@@ -1,8 +1,13 @@
 package com.meli.socialmeli.services.impl;
 
+import com.meli.socialmeli.dtos.response.PostsFromFollowsDTO;
 import com.meli.socialmeli.services.IProductService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements IProductService {
+    @Override
+    public PostsFromFollowsDTO getAllPostsFollowsLastTwoWeeks(Integer userId) {
+        return null;
+    }
 }
