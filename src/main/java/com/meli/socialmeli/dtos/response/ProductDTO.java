b5d1eaefbe3af8dac425a6bb659f4dfb1d9a1d9c
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-15 17:20:28 -0600

    Add DTOS for US006

---
+++ src/main/java/com/meli/socialmeli/dtos/response/ProductDTO.java
@@ -0,0 +1,10 @@
+package com.meli.socialmeli.dtos.response;
+
+public class ProductDTO {
+    private Integer product_id;
+    private String product_name;
+    private String Gamer;
+    private String brand;
+    private String color;
+    private String notes;
+}
