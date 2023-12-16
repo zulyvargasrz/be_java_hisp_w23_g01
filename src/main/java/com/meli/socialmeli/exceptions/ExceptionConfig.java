95b8eec45ec8a5bd72f5aba3917520bc138d1e80
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2023-12-15 20:10:38 -0500

    Add Exceptions
    Data source exception, Exception error with type, Exception handlers

--- src/main/java/com/meli/socialmeli/exceptions/ExceptionConfig.java
+++ src/main/java/com/meli/socialmeli/exceptions/ExceptionConfig.java
@@ -1,11 +1,13 @@
 package com.meli.socialmeli.exceptions;
 
 import com.meli.socialmeli.dtos.response.ExceptionDTO;
+import com.meli.socialmeli.exceptions.custom.DataSourceException;
 import com.meli.socialmeli.exceptions.custom.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ExceptionConfig {
@@ -14,4 +16,17 @@ public class ExceptionConfig {
         ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(DataSourceException.class)
+    public ResponseEntity<ExceptionDTO> handlerDataSoureException(DataSourceException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionDTO> handlerMethodArgument(MethodArgumentTypeMismatchException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage());
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.BAD_REQUEST);
+    }
+
 }
