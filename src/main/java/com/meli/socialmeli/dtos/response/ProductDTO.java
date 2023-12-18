420ca67d9f2fea856b39d45a30e68119c4567a57
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-18 10:26:25 -0600

    Add Lombok getter and constructors

--- src/main/java/com/meli/socialmeli/dtos/response/ProductDTO.java
+++ src/main/java/com/meli/socialmeli/dtos/response/ProductDTO.java
@@ -1,9 +1,16 @@
 package com.meli.socialmeli.dtos.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private Integer product_id;
     private String product_name;
-    private String Gamer;
+    private String type;
     private String brand;
     private String color;
     private String notes;
