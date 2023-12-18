1f3fa5eb612e438f5537339e690849149bfbd82f
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-18 10:43:56 -0600

    Add signature to getFollowersByUser

--- src/main/java/com/meli/socialmeli/repositories/IUserRepository.java
+++ src/main/java/com/meli/socialmeli/repositories/IUserRepository.java
@@ -1,8 +1,12 @@
 package com.meli.socialmeli.repositories;
 
+import com.meli.socialmeli.entities.Post;
 import com.meli.socialmeli.entities.User;
+
+import java.util.List;
 
 
 public interface IUserRepository {
     User finById(int id);
+    List<User> getUserFollowers(User user);
 }
