34ed09a55874dd06442fe6fef0f65d71c131032b
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-18 10:47:13 -0600

    Add signature to get follows by user id

--- src/main/java/com/meli/socialmeli/services/IUserService.java
+++ src/main/java/com/meli/socialmeli/services/IUserService.java
@@ -1,7 +1,11 @@
 package com.meli.socialmeli.services;
 
 import com.meli.socialmeli.dtos.response.UserFollowersDTO;
+import com.meli.socialmeli.entities.User;
+
+import java.util.List;
 
 public interface IUserService {
     UserFollowersDTO findFollowersById(int id);
+    List<User> findFollowsByIdProductService(int id);
 }
