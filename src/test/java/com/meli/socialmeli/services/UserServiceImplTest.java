f4c98fc341bbaa09bfece32da96e2d000cc9965c
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2023-12-27 16:19:58 -0500

    Add T0003 merge with dev

--- src/test/java/com/meli/socialmeli/services/UserServiceImplTest.java
+++ src/test/java/com/meli/socialmeli/services/UserServiceImplTest.java
@@ -8,16 +8,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.meli.socialmeli.dtos.response.MessageDTO;
-import com.meli.socialmeli.entities.User;
-import com.meli.socialmeli.exceptions.custom.BadRequestException;
 import com.meli.socialmeli.exceptions.custom.DataSourceException;
 import com.meli.socialmeli.repositories.IUserRepository;
 import com.meli.socialmeli.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -44,54 +40,6 @@ class UserServiceImplTest {
 
     @InjectMocks
     UserServiceImpl userService;
-
-    @Test
-    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - name_asc")
-    void getFollowersListByNameAscShouldReturnSortedList() {
-        //Arrange
-        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
-        when(userRepository.finById(100000)).thenReturn(userFromRepository);
-
-        UserFollowersDTO userFollowersExpected = UserDTOUtilsGenerator.getUserFollowersDTOWithThreeFollowersOrderAsc();
-
-        //Act
-        UserFollowersDTO resultUserFollowers = userService.findFollowersById(100000, "name_asc");
-
-        //Assert
-        assertEquals(userFollowersExpected.getUser_id(),resultUserFollowers.getUser_id());
-        assertEquals(userFollowersExpected.getUser_name(),resultUserFollowers.getUser_name());
-        assertEquals(userFollowersExpected.getFollowers(),resultUserFollowers.getFollowers());
-    }
-
-    @Test
-    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - name_desc")
-    void getFollowersListByNameDescShouldReturnSortedList() {
-        //Arrange
-        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
-        when(userRepository.finById(100000)).thenReturn(userFromRepository);
-
-        UserFollowersDTO userFollowersExpected = UserDTOUtilsGenerator.getUserFollowersDTOWithThreeFollowersOrderDesc();
-
-        //Act
-        UserFollowersDTO resultUserFollowers = userService.findFollowersById(100000, "name_desc");
-
-        //Assert
-        assertEquals(userFollowersExpected.getUser_id(),resultUserFollowers.getUser_id());
-        assertEquals(userFollowersExpected.getUser_name(),resultUserFollowers.getUser_name());
-        assertEquals(userFollowersExpected.getFollowers(),resultUserFollowers.getFollowers());
-    }
-
-    @Test
-    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - Orden no válido")
-    void getFollowersListByNameEmptyShouldThrowException() {
-        //Arrange
-        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
-        when(userRepository.finById(100000)).thenReturn(userFromRepository);
-
-        //Act - Assert
-        assertThrows(BadRequestException.class, ()->userService.findFollowersById(100000, "empty"));
-    }
-
 
     private List<User> loadTestUsers(){
         List<User> userList;
@@ -205,5 +153,52 @@ class UserServiceImplTest {
             userService.followSeller(userIdFollower, userIdFollowed);
         });
         Assertions.assertEquals(expectedMessage, e.getMessage());
+    }
+
+    @Test
+    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - name_asc")
+    void getFollowersListByNameAscShouldReturnSortedList() {
+        //Arrange
+        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
+        when(userRepository.finById(100000)).thenReturn(userFromRepository);
+
+        UserFollowersDTO userFollowersExpected = UserDTOUtilsGenerator.getUserFollowersDTOWithThreeFollowersOrderAsc();
+
+        //Act
+        UserFollowersDTO resultUserFollowers = userService.findFollowersById(100000, "name_asc");
+
+        //Assert
+        assertEquals(userFollowersExpected.getUser_id(),resultUserFollowers.getUser_id());
+        assertEquals(userFollowersExpected.getUser_name(),resultUserFollowers.getUser_name());
+        assertEquals(userFollowersExpected.getFollowers(),resultUserFollowers.getFollowers());
+    }
+
+    @Test
+    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - name_desc")
+    void getFollowersListByNameDescShouldReturnSortedList() {
+        //Arrange
+        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
+        when(userRepository.finById(100000)).thenReturn(userFromRepository);
+
+        UserFollowersDTO userFollowersExpected = UserDTOUtilsGenerator.getUserFollowersDTOWithThreeFollowersOrderDesc();
+
+        //Act
+        UserFollowersDTO resultUserFollowers = userService.findFollowersById(100000, "name_desc");
+
+        //Assert
+        assertEquals(userFollowersExpected.getUser_id(),resultUserFollowers.getUser_id());
+        assertEquals(userFollowersExpected.getUser_name(),resultUserFollowers.getUser_name());
+        assertEquals(userFollowersExpected.getFollowers(),resultUserFollowers.getFollowers());
+    }
+
+    @Test
+    @DisplayName("T-0003:Verificar que el tipo de ordenamiento alfabético exista. Continuar con normalidad - Orden no válido")
+    void getFollowersListByNameEmptyShouldThrowException() {
+        //Arrange
+        User userFromRepository = UserEntityUtilsGenerator.getUserWithThreeFollowers();
+        when(userRepository.finById(100000)).thenReturn(userFromRepository);
+
+        //Act - Assert
+        assertThrows(BadRequestException.class, ()->userService.findFollowersById(100000, "empty"));
     }
 }
