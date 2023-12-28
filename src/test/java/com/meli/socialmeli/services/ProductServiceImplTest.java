ea293c5a5881895ecdb804a22ab13d48815d2255
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-28 09:03:48 -0600

    Add correction for T-0005 good path

--- src/test/java/com/meli/socialmeli/services/ProductServiceImplTest.java
+++ src/test/java/com/meli/socialmeli/services/ProductServiceImplTest.java
@@ -2,12 +2,8 @@ package com.meli.socialmeli.services;
 
 import com.meli.socialmeli.entities.User;
 import com.meli.socialmeli.exceptions.custom.BadRequestException;
-import com.meli.socialmeli.repositories.IProductRepository;
-import com.meli.socialmeli.repositories.IUserRepository;
 import com.meli.socialmeli.services.impl.ProductServiceImpl;
 import com.meli.socialmeli.services.impl.UserServiceImpl;
-import org.assertj.core.api.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import util.UserEntityUtilsGenerator;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+import static util.UserEntityUtilsGenerator.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
@@ -37,10 +36,8 @@ class ProductServiceImplTest {
     void getAllPostsFollowsLastTwoWeeksOrdenamientoValidoTest(){
         // Arrange
         String order = "date_asc";
-        User user = new User(1,"User", List.of(), List.of(), List.of());
-        User seller1 = new User(2, "seller1", List.of(), List.of(), List.of());
-        User seller2 = new User(3, "seller2", List.of(), List.of(), List.of());
-        List<User> sellers = List.of(seller1, seller2);
+        User user = getUserFollwingSellers();
+        List<User> sellers = user.getFollowers();
         Integer id = user.getUser_id();
         // Act
         when(userService.findFollowsByIdProductService(Mockito.anyInt())).thenReturn(sellers);
