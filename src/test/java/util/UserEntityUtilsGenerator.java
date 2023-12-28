e8f9665f6f051c8ba900e2a5c734bae6b8f5886f
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-28 09:05:15 -0600

    Add utility for T-0005 good path

--- src/test/java/util/UserEntityUtilsGenerator.java
+++ src/test/java/util/UserEntityUtilsGenerator.java
@@ -1,7 +1,10 @@
 package util;
 
+import com.meli.socialmeli.entities.Post;
+import com.meli.socialmeli.entities.Product;
 import com.meli.socialmeli.entities.User;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class UserEntityUtilsGenerator {
@@ -26,6 +29,50 @@ public class UserEntityUtilsGenerator {
         userFollowerOne.setFollowers(List.of(user));
         userFollowerTwo.setFollowers(List.of(user));
         userFollowerThree.setFollowers(List.of(user));
+        return user;
+    }
+
+    public static User getUserFollwingSellers() {
+        User seller1 = new User(2, "seller1", List.of(), List.of(), List.of());
+        User seller2 = new User(3, "seller2", List.of(), List.of(), null);
+        List sellers = List.of(seller1, seller2);
+
+        Post post1 = new Post(
+                LocalDate.of(2023, 12, 20),
+                new Product(
+                        1,
+                        "Compu Gamer",
+                        "Compu",
+                        "MSI",
+                        "Black",
+                        "Compu buena"
+                ),
+                1,
+                10000.0,
+                1
+        );
+
+        Post post2 = new Post(
+                LocalDate.of(2023, 12, 11),
+                new Product(
+                        2,
+                        "Silla Ergonomica",
+                        "Silla",
+                        "Coleman",
+                        "Azul",
+                        "Buena silla"
+                ),
+                1,
+                154454.4,
+                2
+        );
+
+        List<Post> posts = List.of(post1, post2);
+
+        seller2.setPosts(posts);
+
+        User user = new User(1,"User", sellers, List.of(), List.of());
+
         return user;
     }
 }
