14d64d8064ceeab073dcc49466b9b612bddd4b7b
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-28 11:12:46 -0600

    Add map methods (refactor)

--- src/main/java/com/meli/socialmeli/utilities/Mappers.java
+++ src/main/java/com/meli/socialmeli/utilities/Mappers.java
@@ -1,9 +1,11 @@
 package com.meli.socialmeli.utilities;
 
 import com.meli.socialmeli.dtos.request.PostDTO;
+import com.meli.socialmeli.dtos.response.PostNoPromoDTO;
 import com.meli.socialmeli.dtos.response.ProductDTO;
 import com.meli.socialmeli.entities.Post;
 import com.meli.socialmeli.entities.Product;
+import com.meli.socialmeli.entities.User;
 
 public class Mappers {
 
@@ -13,5 +15,27 @@ public class Mappers {
     }
     public static Post mapNewPost(PostDTO post, int post_id){
         return new Post(post.getDate(), mapProduct(post.getProduct()), post.getCategory(), post.getPrice(), post_id);
+    }
+
+    public static ProductDTO mapProductProductDTO(Product product){
+        return new ProductDTO(
+                product.getProduct_id(),
+                product.getProduct_name(),
+                product.getType(),
+                product.getBrand(),
+                product.getColor(),
+                product.getNotes()
+        );
+    }
+
+    public static PostNoPromoDTO mapPostUserAndProductPromoNoPromoDTO(Post post, User user) {
+        return new PostNoPromoDTO(
+                user.getUser_id(),
+                post.getPost_id(),
+                post.getDate().toString(),
+                mapProductProductDTO(post.getProduct()),
+                post.getCategory(),
+                post.getPrice()
+        );
     }
 }
